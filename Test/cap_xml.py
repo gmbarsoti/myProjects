6b76e153a367576a48d5b5e522dc38d358dc9394
Guilherme <gmbarsoti@gmail.com>
2018-06-28 19:14:09 -0300

    Func to create cap.txt from xml - Func to extract all ctec from a dir

--- Test/cap_xml.py
+++ Test/cap_xml.py
@@ -42,27 +42,27 @@ class captured_config:
         print("Info: ", self.Info)
         print("lines number: ", self.Total_lines)
         print(self.communication_lines[1])
-        print(self.communication_lines[3462])
+        #print(self.communication_lines[3462])
         
     def get_communication(self):
         return self.communication_lines
 
 def generate_xml(cap_file):
-    data_folder = Path("D:/sand_box/")
-    program_path = data_folder / "ProtocolAnalyzerSaveXml.exe"
     
-    data_folder = Path("D:/sand_box/")
+    root_path = Path("D:/sand_box/")
+    program_path = root_path / "ProtocolAnalyzerSaveXml.exe"
     
-    cap_path = data_folder / cap_file
-    print(program_path)
-    print (cap_path)
+    
+    cap_files_path = Path("D:/sand_box/cap_files/")
+    cap_path = cap_files_path / cap_file
+    
     run([str(program_path),str(cap_path)], stdout=PIPE)
 
 
 
 def xml_treatment():
     data_folder = Path("D:/sand_box/")
-    program_path = data_folder /  "InitGscan.xml"
+    program_path = data_folder /  "Leitutas.xml"
     
     tree = ET.parse(program_path)
     root = tree.getroot()
@@ -91,11 +91,53 @@ def xml_treatment():
         com_list.append(dict(one_com_dict)) 
         
     return xml_dict, com_list
+
+
+def occurred_cases2(full_vector):
+    ''' Return a list with just an occurrence of each service request 
+    that is made in a comunication list'''
+    ret_vector = [] # list that returns a once occurred request and its response
+    services_occurred = [] # list to store services that have already been found
     
+    for item in full_vector:
+        service = item[0][2] # getting just the service
+        if not service in services_occurred: # if service was not found yet
+            services_occurred.append(service) # record this service occurrence
+            ret_vector.append(item) # store request and response
+       
+    return ret_vector  
+
+def cap_files():
+    ''' Return a list with all path to cap files from a specific directory'''
+    if not os.path.isdir("D:/sand_box/cap_files"):
+        print("Missing cap_file directory...")
+        print("Missing captured files...")
+        print("Creating directory cap_files...")
+        os.makedirs("D:/sand_box/cap_files")
+   
+    all_cap_path = os.listdir("D:/sand_box/cap_files")
+    return all_cap_path
+
+def move_xml_files():
+    dir_files = os.listdir("D:/sand_box/cap_files")
     
+    for item in dir_files:
+        if item[-3:] == "xml":
+            os.rename("D:/sand_box/cap_files/" + item, "D:/sand_box/cap_files/xml_files/" + item)
+
 def main():
     
-    cap_file = "InitGscan.ctec"
+    #cap_file = "Leitutas.ctec"
+    
+    all_cap_file = cap_files()
+    
+    for cap_file in all_cap_file:
+        print(cap_file)
+        generate_xml(cap_file)
+    
+    move_xml_files()
+    input()
+    cap_file = "Leitutas.ctec"    
     generate_xml(cap_file)
     
     xml_dict, com_list = xml_treatment()
@@ -106,14 +148,27 @@ def main():
     if not os.path.isdir("D:/sand_box/output"):
         print("Creating directory output...")
         os.makedirs("D:/sand_box/output")
+    
+    
+    
+    for cap_file in all_cap_file:
+        f = open("D:/sand_box/output/" + cap_file,"a")
         
     f = open("D:/sand_box/output/cap.txt","a")
-    l = a_class.get_communication()
-    line_to_write = (l[0]["Time"] + " " + l[0]["Count"] + " "  + 
-                     l[0]["Format"] + " "  + l[0]["Id"] + " "  + 
-                     str(l[0]["Data"]).replace(',','.') + "\n") 
-    f.write(line_to_write)
-    f.close()
+    communication = a_class.get_communication()
     
+    
+    
+    # Putting all lines to the main file
+    for com in communication:
+        line_to_write = (com["Format"] + " "  + com["Id"] + "."  + 
+                     str(com["Data"]).replace(',','.') + "\n") 
+        f.write(line_to_write)
+    f.close()
+    #===========================================================================
+    # line_to_write = (l[0]["Time"] + " " + l[0]["Count"] + " "  + 
+    #                  l[0]["Format"] + " "  + l[0]["Id"] + " "  + 
+    #                  str(l[0]["Data"]).replace(',','.') + "\n") 
+    #===========================================================================
 if __name__ == "__main__":
     main()
